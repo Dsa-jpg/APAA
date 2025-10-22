e8d221303e814e8f92667c9d9802a2f5f48801cd
Filip <nachtman.filip@seznam.cz>
2025-10-22 21:24:19 +0200

    added step learning for LNU

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from neuron.linear_neuron import LinearNeuron
 
 data = DataGenerator(a1=100, a2=20, f1=250, f2=1000)
 t, signal = data.generate()
-LNU = LinearNeuron(learning_rate=0.5, signal=signal, t=t, look_back_window=5)
 normalized_signal = data.normalize(signal)
+LNU = LinearNeuron(learning_rate=0.1, signal=normalized_signal, t=t, look_back_window=5)
+LNU.learn(epochs=2)
 
 if __name__ == '__main__':
-    plt.figure(figsize=(10, 4))
-    # plt.plot(t, signal, label="Original Signal")
+    plt.figure(figsize=(10, 6))
+    plt.subplot(2, 1, 1)
+    plt.plot(t, signal, label="Original Signal")
+    plt.subplot(2, 1, 2)
     plt.plot(t, normalized_signal, label="Normalized Signal (Z-score)")
     plt.xlabel("Time [s]")
     plt.ylabel("Amplitude")
--- neuron/linear_neuron.py
+++ neuron/linear_neuron.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 
 
@@ -5,17 +7,57 @@ class LinearNeuron(object):
 
     def __init__(self, learning_rate: float, signal, t, look_back_window):
         self.learning_rate = learning_rate
-        self.weights = np.random.random(signal.shape) * 2 - 1
         self.signal = signal
         self.t = t
         self.look_back_window = look_back_window
+        # Generates random weights in interval [-1,1]
+        self.weights = np.random.random(self.look_back_window) * 2 - 1
 
-        ...
+    def _get_error_for_sample(self, measured_signal, predicted_signal):
+        """
+        Does substitution of  measured_signal and predicted_signal.
 
-    def learn(self):
-        ...
+        :param measured_signal:
+        :param predicted_signal:
+        :return: error_for_sample
+        """
+        return measured_signal - predicted_signal
 
-    def _get_error_for_sample(self, measured_signal, predicted_signal, time):
-        ...
+    def _update_weights(self, e, x_vector):
+        """
+        Calculates the new weights based on the error and x_vector.
+        Using backpropagation.
 
-    ...
+        :param e:
+        :param x_vector:
+        :return: None
+        """
+        self.weights += self.learning_rate * e * x_vector
+
+    def _predict(self, x_vector):
+        """
+        Sum of input of x_vector * weights.
+
+        :param x_vector:
+        :return:
+        """
+        return np.dot(x_vector, self.weights)
+
+    def learn(self, epochs=10, debug=False):
+        for epoch in range(epochs):
+            total_error = 0
+
+            for k in range(self.look_back_window, len(self.signal)):
+                x_vec = self.signal[k - self.look_back_window:k]  # takes last n samples before k for neurons output
+                y = self.signal[k]  # value that is being predicted by LNU
+
+                y_predict = self._predict(x_vec)
+                error = self._get_error_for_sample(y, y_predict)
+
+                self._update_weights(error, x_vec)
+                total_error += error ** 2
+                if debug:
+                    print(f"predicted_value: {y_predict}, actual_value: {y} error: {error}")
+                    time.sleep(self.learning_rate)
+
+            print(f"epoch: {epoch + 1}, total_error: {total_error:.4f}")
